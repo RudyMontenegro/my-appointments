a831b3ffd39f8e81973daaecad6a8eb538bfbcb4
rolidoss <rudymontenegro4@gmail.com>
2021-07-19 23:23:47 -0400

    crud de especialidades

--- my-appointments/resources/views/includes/panel/menu.blade.php
+++ my-appointments/resources/views/includes/panel/menu.blade.php
@@ -3,22 +3,22 @@
 <h6 class="navbar-heading text-muted">Gestionar Datos</h6>
 <ul class="navbar-nav">
     <li class="nav-item">
-        <a class="nav-link" href="./index.html">
+        <a class="nav-link" href="/home">
             <i class="ni ni-tv-2 text-primary"></i> Dashboard
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="./examples/icons.html">
+        <a class="nav-link" href="{{ url('specialties')}}">
             <i class="ni ni-planet text-blue"></i>Especialidades
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="./examples/maps.html">
+        <a class="nav-link" href="{{ url('doctors')}}">
             <i class="ni ni-single-02 text-blue"></i>Medicos 
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="./examples/profile.html">
+        <a class="nav-link" href="{{ url('patients')}}">
             <i class="ni ni-satisfied text-blue"></i> Pacientes 
         </a>
     </li>
@@ -39,12 +39,12 @@
 <!-- Navigation -->
 <ul class="navbar-nav mb-md-3">
     <li class="nav-item">
-        <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html">
+        <a class="nav-link" href="#">
             <i class="ni ni-collection"></i> Frecuencia de Citas
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html">
+        <a class="nav-link" href="#">
             <i class="ni ni-sound-wave"></i> Medico mas activos
         </a>
     </li>
--- my-appointments/resources/views/layouts/panel.blade.php
+++ my-appointments/resources/views/layouts/panel.blade.php
@@ -51,7 +51,7 @@
                         aria-expanded="false">
                         <div class="media align-items-center">
                             <span class="avatar avatar-sm rounded-circle">
-                                <img alt="Image placeholder" src="{{ asset('img/theme/favicon.png') }}">
+                                <img alt="Image placeholder" src="{{ asset('img/brand/favicon.png') }}">
                             </span>
                         </div>
                     </a>
@@ -88,7 +88,8 @@
         <nav class="navbar navbar-top navbar-expand-md navbar-dark" id="navbar-main">
             <div class="container-fluid">
                 <!-- Brand -->
-                <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="./index.html">Dashboard</a>
+                <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="./index.html">Panel de
+                    Administrador</a>
                 <!-- Form -->
                 <form class="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
                     <div class="form-group mb-0">
@@ -107,8 +108,8 @@
                             aria-expanded="false">
                             <div class="media align-items-center">
                                 <!-- <span class="avatar avatar-sm rounded-circle">
-                  <img alt="Image placeholder" src=" {{ asset('img/theme/team-4-800x800.jpg')}} ">
-                </span>-->
+                                 <img alt="Image placeholder" src=" {{ asset('img/theme/team-4-800x800.jpg')}} ">
+                                     </span>-->
                                 <div class="media-body ml-2 d-none d-lg-block">
                                     <span class="mb-0 text-sm  font-weight-bold">{{ auth()->user()->name }}</span>
                                 </div>
